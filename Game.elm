b0f5dfa22d0a065df92e8ad1237a48d5a8bba605
David Crespo <crespo.dm@gmail.com>
2014-10-12 11:21:23 -0500

    there is now a platform and everything is horribly complicated

--- Game.elm
+++ Game.elm
@@ -2,8 +2,15 @@ import Dict (Dict, get, fromList)
 import Keyboard (KeyCode, space, isDown)
 
 type CannonNum = Float
-type BulletPos = Float
 type KeyDown = Bool
+data Dir = Left | Right
+
+type BulletColState = [Float]
+type BulletsState = [BulletColState]
+
+type PlatformState = { dir:Dir, pos:Float, len:Float }
+
+type GameState = ([PlatformState], BulletsState)
 
 numCannons = 8
 cannonSpacing = 70
@@ -17,42 +24,59 @@ input : Signal [KeyDown]
 input = sampleOn (fps 30) (combine (map isDown homerow))
 
 --main = lift asText (foldp step bulletState input)
-main = let emptyBulletState = repeat numCannons []
-       in lift displayBullets (foldp step emptyBulletState input)
+main = let initBulletsState = repeat numCannons []
+           initPlatformsState = [{ dir=Left, pos=0, len=100 }]
+           initState = (initPlatformsState, initBulletsState)
+       in lift displayBullets (foldp step initState input)
 
 -- STEP
 
-step keys = bulletsMove2 >> maybeAddBullet2 keys
+step : [KeyDown] -> GameState -> GameState
+step keys = bulletsMove2 >> maybeAddBullet2 keys >> platformsMove2
 
 bulletSpeed = 5
+platformSpeed = 5
 
-maybeAddBullet2 : [KeyDown] -> [[BulletPos]] -> [[BulletPos]]
-maybeAddBullet2 ps bbs = map maybeAddBullet (zip ps bbs)
+maybeAddBullet2 : [KeyDown] -> GameState -> GameState
+maybeAddBullet2 keys (ps, bbs) = (ps, map maybeAddBullet (zip keys bbs))
 
-maybeAddBullet : (KeyDown, [BulletPos]) -> [BulletPos]
+maybeAddBullet : (KeyDown, BulletColState) -> BulletColState
 maybeAddBullet (p,bs) = if p then (cannonH :: bs) else bs
 
-bulletsMove2 : [[BulletPos]] -> [[BulletPos]]
-bulletsMove2 = map bulletsMove
+bulletsMove2 : GameState -> GameState
+bulletsMove2 (ps, bbs) = (ps, map bulletsMove bbs)
 
-bulletsMove : [BulletPos] -> [BulletPos]
+bulletsMove : BulletColState -> BulletColState
 bulletsMove xs = case xs of
                   [] -> []
                   (b::bs) -> if b < gameH
                              then (b + bulletSpeed) :: bulletsMove bs
                              else bulletsMove bs
 
+platformsMove2 : GameState -> GameState
+platformsMove2 (ps, bbs) = (platformsMove ps, bbs)
+
+platformsMove : [PlatformState] -> [PlatformState]
+platformsMove xs = case xs of
+                     [] -> []
+                     (p::ps) -> if p.pos < gameW
+                                then { p | pos <- p.pos + platformSpeed } :: platformsMove ps
+                                else platformsMove ps
 
 -- DISPLAY CONFIG
 
 myGrey = rgba 240 240 240 1
 cannonColor = blue
 bulletColor = black
+platformColor = green
+
 (gameW,gameH) = ((numCannons + 1) * cannonSpacing, 400)
 (halfGameW,halfGameH) = (gameW/2,200)
+
 (bulletW, bulletH) = (2, 6)
 (cannonW, cannonH) = (6, 14)
 (halfCannonW, halfCannonH) = (3, 7)
+platformH = 20
 
 greyBackground = rect gameW gameH |> filled myGrey
 
@@ -62,19 +86,30 @@ cannonXOffset n = n * cannonSpacing - halfGameW
 
 -- DISPLAY
 
-displayBullets : [[BulletPos]] -> Element
-displayBullets bbs = collage gameW gameH (
-                         greyBackground
-                         :: (map cannon [1..numCannons])
-                         ++ (concatMap bullets (zip [1..numCannons] bbs)))
+displayBullets : GameState -> Element
+displayBullets (ps,bbs) = collage gameW gameH (
+                           greyBackground
+                           :: (map cannon [1..numCannons])
+                           ++ (concatMap bullets (zip [1..numCannons] bbs))
+                           ++ (map platform ps))
 
-bullets : (CannonNum, [BulletPos]) -> [Form]
+bullets : (CannonNum, BulletColState) -> [Form]
 bullets (n, bs) = map (bullet n) bs
 
-bullet : CannonNum -> BulletPos -> Form
+bullet : CannonNum -> Float -> Form
 bullet n pos = move (cannonXOffset n, halfGameH - pos)
                     (rect bulletW bulletH |> filled bulletColor)
 
 cannon : CannonNum -> Form
 cannon n = move (cannonXOffset n, halfGameH - halfCannonH)
                 (rect cannonW cannonH |> filled cannonColor)
+
+platform : PlatformState -> Form
+platform {dir,pos,len} = let dm = multiplier dir in
+                         move (dm * pos, 0)
+                         (rect len platformH |> filled platformColor)
+
+multiplier : Dir -> Float
+multiplier d = case d of
+                 Left -> -1
+                 Right -> 1

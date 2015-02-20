3b50efd0230e8fef3b21f10b2f32735474803a55
David Crespo <crespo.dm@gmail.com>
2015-02-20 13:35:43 -0600

    scratch can count up seconds. wow

--- Scratch.elm
+++ Scratch.elm
@@ -2,9 +2,10 @@ import Graphics.Element (..)
 import Text (..)
 import Random (int, generate, initialSeed, Seed)
 import Time (Time, every, second, fps, timestamp)
-import Signal (Signal, constant)
+import Signal
 
 type alias GameState = { seed : Seed }
+type alias CountState = Int
 
 -- getTime : Time
 -- getTime = fst <| timestamp <| constant 5
@@ -13,5 +14,12 @@ randInt : Int -> Int -> GameState -> (Int, GameState)
 randInt a b gs = let (i, seed') = generate (int a b) gs.seed
                  in (i, {gs | seed <- seed' } )
 
-main : Element
-main = asText <| fst <| randInt 1 10 { seed = initialSeed 5 }
+inc : Time -> Int -> Int
+inc _ = (+) 1
+
+
+-- main : Signal Element
+-- main = Signal.map asText (Signal.foldp inc 1 (every second))
+
+main : Signal Element
+main = Signal.map asText (every second)
